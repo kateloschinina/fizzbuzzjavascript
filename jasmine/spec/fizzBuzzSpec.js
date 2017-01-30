395f9df744ad5ec93d4ee2b03f8399c56903695c
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-2.local>
2017-01-30 19:24:06 +0000

    Created Divisible functions for all apropriate occasions.

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -4,11 +4,27 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
 
-    it('divisible by 3', function() {
-
+    beforeEach(function() {
       javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-
     });
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+  });
+
+  describe('returns Fizz and Buzz in right situations', function() {
+    
   });
 });
